4b269523cb915c6a6208fa0eac7570821b4d9758
admin <alexandrpeks@gmail.com>
2024-05-05 21:36:21 +0400

    Добавлены описания к модулям, документация приведена в один вид для всех модулей.

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+"""
+       Модуль предназначен для запуска Flask-приложения при выполнении
+       файла как основной программы.
+
+       Импортирует объект app из модуля controller и модуль errors для
+       регистрации обработчиков ошибок.
+"""
+
 if __name__ == '__main__':
     from controller import app
     from errors import app
-    app.run(port=5003, debug=True)
+    app.run()
